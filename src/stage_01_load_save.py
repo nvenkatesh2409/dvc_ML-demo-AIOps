98b261041d3f284adc1ee8d553df35575d1bfeb8
Venkatesh <vn64@dxc.com>
2021-12-26 11:33:22 +0530

    stage 02 added

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -7,7 +7,6 @@ def get_data(config_path):
     config = read_yaml(config_path)
     remote_data_path = config["data_source"]
     df = pd.read_csv(remote_data_path,sep=";")
-    
     # save dataset in the local directory
     # create path to directory: artifacts/raw_local_dir/data.csv
     artifacts_dir = config["artifacts"]["artifact_dir"]
